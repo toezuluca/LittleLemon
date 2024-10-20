c6716d3a6fb9785c057927b484a8f0a20cf75579
theo <theodor.oezuluca@tum.de>
2024-10-20 16:41:14 +0200

    added token-based authentication w/ djoser

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -40,7 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
+
+DJOSER={"USER_ID_FIELD":"username"}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -95,6 +106,9 @@ DATABASES = {
 #         }   
 #     }   
 # }
+
+
+
 
 
 # Password validation
--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Booking, Menu
+# from rest_framework.authtoken.admin import TokenAdmin
+# from rest_framework.authtoken.models import Token
+
 
 # Register your models here.
 admin.site.register(Booking)
